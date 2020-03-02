1f8c19b9f328c481ea54907273ac89dd461daa2b
B. Schmidt <mynyxi@gmail.com>
2020-03-02 21:33:16 +0100

    Added react-native-chart-kit with example

--- src/screens/GraphScreen.js
+++ src/screens/GraphScreen.js
@@ -8,14 +8,15 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  Button,
-  StyleSheet,
-  ScrollView,
-  TouchableOpacity,
+  Dimensions,
   Text,
+  SafeAreaView,
+  ScrollView,
   StatusBar,
 } from 'react-native';
+import {
+  LineChart
+} from "react-native-chart-kit";
 import AsyncStorage from '@react-native-community/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -34,8 +35,50 @@ class GraphScreen extends React.Component {
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic">
-
-          
+          <Text>Sample line graph</Text>
+          <LineChart
+            data={{
+              labels: ["January", "February", "March", "April", "May", "June"],
+              datasets: [
+                {
+                  data: [
+                    Math.random() * 100,
+                    Math.random() * 100,
+                    Math.random() * 100,
+                    Math.random() * 100,
+                    Math.random() * 100,
+                    Math.random() * 100
+                  ]
+                }
+              ]
+            }}
+            width={Dimensions.get("window").width} // from react-native
+            height={220}
+            yAxisLabel="$"
+            yAxisSuffix="k"
+            yAxisInterval={1} // optional, defaults to 1
+            chartConfig={{
+              backgroundColor: "#e26a00",
+              backgroundGradientFrom: "#fb8c00",
+              backgroundGradientTo: "#ffa726",
+              decimalPlaces: 2, // optional, defaults to 2dp
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16
+              },
+              propsForDots: {
+                r: "6",
+                strokeWidth: "2",
+                stroke: "#ffa726"
+              }
+            }}
+            bezier
+            style={{
+              marginVertical: 8,
+              borderRadius: 16
+            }}
+          />
         </ScrollView>
       </SafeAreaView>
       </>
