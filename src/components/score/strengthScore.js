6d30d72e8c1c91a69186b97dcb152d266fe54d60
B. Schmidt <mynyxi@gmail.com>
2020-03-08 11:17:49 +0100

    Refactoring strength score calculation

--- src/components/score/strengthScore.js
+++ src/components/score/strengthScore.js
@@ -2,7 +2,11 @@
  * Credit: https://symmetricstrength.com/
  */
 
+ /**
+  * @private
+  */
 var wilksToStrengthScore = function(expectedWilks, age) {
+  "use strict";
   var ageModifier=1;
   if(typeof age == "number" && age < 23) {
     ageModifier = (0.0038961 * Math.pow(age,2)) - 0.166926 * age + 2.80303;
@@ -13,14 +17,20 @@ var wilksToStrengthScore = function(expectedWilks, age) {
   return expectedWilks*ageModifier/4;
 };
 
-var wilksCoefficient = function(sex, bodyweight){
+/**
+ * @private
+ * @param {integer} age - The age of the lifter in years
+ * @param {integer} bodyweight - The bodyweight of the lifter in kilogram
+ */
+var wilksCoefficient = function(isMale, bodyweight) {
+  "use strict";
   var a=594.31747775582,
       b=-27.23842536447,
       c=0.82112226871,
       d=-0.00930733913,
       e=4731582e-11,
       f=-9.054e-8;
-  if(sex === "Male"){
+  if(isMale){
     a=-216.0475144;
     b=16.2606339;
     c=-0.002388645;
@@ -31,33 +41,38 @@ var wilksCoefficient = function(sex, bodyweight){
   return 500/(a + b*bodyweight + c*Math.pow(bodyweight,2) + d*Math.pow(bodyweight,3) + e*Math.pow(bodyweight,4) + f*Math.pow(bodyweight,5));
 };
 
-var percentOfPLTotal = function(unitSystem, sex, bodyweight, exerciseName, oneRep){
+/**
+ * @private
+ */
+var percentOfPLTotal = function(isMale, bodyweight, exerciseName, oneRep) {
+  "use strict";
+
   var deadliftMale = 0.396825,
       deadliftFemale = 0.414938;
   switch(exerciseName){
     case "sumoDeadlift":
-    case "deadlift": return (sex === "Male")? deadliftMale : deadliftFemale;
-    case "backSquat": return (sex === "Male")? 0.87*deadliftMale : 0.84*deadliftFemale;
-    case "benchPress": return (sex === "Male")? 0.65*deadliftMale : 0.57*deadliftFemale;
-    case "powerClean": return (sex === "Male")? 0.56*deadliftMale : 0.56*deadliftFemale;
-    case "frontSquat": return (sex === "Male")? 0.8*0.87*deadliftMale : 0.8*0.84*deadliftFemale;
-    case "inclineBenchPress": return (sex === "Male")? 0.82*0.65*deadliftMale : 0.82*0.57*deadliftFemale;
-    case "overheadPress": return (sex === "Male")? 0.65*0.65*deadliftMale : 0.65*0.57*deadliftFemale;
-    case "pushPress": return (sex === "Male")? 1.33*0.65*0.65*deadliftMale : 1.33*0.65*0.57*deadliftFemale;
-    case "snatchPress": return (sex === "Male")? 0.8*0.65*0.65*deadliftMale : 0.8*0.65*0.57*deadliftFemale;
-    case "pendlayRow": return (sex === "Male")? 0.53*deadliftMale : 0.53*deadliftFemale;
+    case "deadlift": return isMale? deadliftMale : deadliftFemale;
+    case "backSquat": return isMale? 0.87*deadliftMale : 0.84*deadliftFemale;
+    case "benchPress": return isMale? 0.65*deadliftMale : 0.57*deadliftFemale;
+    case "powerClean": return isMale? 0.56*deadliftMale : 0.56*deadliftFemale;
+    case "frontSquat": return isMale? 0.8*0.87*deadliftMale : 0.8*0.84*deadliftFemale;
+    case "inclineBenchPress": return isMale? 0.82*0.65*deadliftMale : 0.82*0.57*deadliftFemale;
+    case "overheadPress": return isMale? 0.65*0.65*deadliftMale : 0.65*0.57*deadliftFemale;
+    case "pushPress": return isMale? 1.33*0.65*0.65*deadliftMale : 1.33*0.65*0.57*deadliftFemale;
+    case "snatchPress": return isMale? 0.8*0.65*0.65*deadliftMale : 0.8*0.65*0.57*deadliftFemale;
+    case "pendlayRow": return isMale? 0.53*deadliftMale : 0.53*deadliftFemale;
   }
 
-  let l="Imperial"===unitSystem?oneRep-bodyweight: 2.20462*(oneRep-bodyweight);
+  let l = 2.20462*(oneRep-bodyweight);
   if("dip"===exerciseName) {
-      if(sex === "Male"){
+      if(isMale){
         return 1.68064e-10*Math.pow(l,4)-1.2946e-7*Math.pow(l,3)+371905e-10*Math.pow(l,2)-0.00499168*l+0.566576;
       }
       return 8.249e-10*Math.pow(l,4)-4.01956e-7*Math.pow(l,3)+622122e-10*Math.pow(l,2)-0.00431442*l+0.37562;
     }
 
   var chinUpPercent = 1.66589e-9*Math.pow(l,4)-5.1621e-7*Math.pow(l,3)+54088e-9*Math.pow(l,2)-0.00281674*l+0.302005;
-  if(sex === "Male"){
+  if(isMale){
       chinUpPercent = 4.01897e-10*Math.pow(l,4)-2.34536e-7*Math.pow(l,3)+502252e-10*Math.pow(l,2)-0.00502633*l+0.459545;
   }
 
@@ -71,15 +86,34 @@ var percentOfPLTotal = function(unitSystem, sex, bodyweight, exerciseName, oneRe
   return 1;
 };
 
-var expectedPLTotal = function(unitSystem, sex, bodyweight, exercise, oneRM){
-  return oneRM/percentOfPLTotal(unitSystem, sex, bodyweight, exercise, oneRM);
+/**
+ * @private
+ */
+var expectedPLTotal = function(isMale, bodyweight, exerciseName, oneRM){
+  "use strict";
+  return oneRM/percentOfPLTotal(isMale, bodyweight, exerciseName, oneRM);
 };
 
-var singleLiftStrengthScore = function(sex, age, bodyweight, exerciseName, oneRM) {
-  var expectedWilks = wilksCoefficient(sex, bodyweight) * expectedPLTotal("Kilogram", sex, bodyweight, exercise, oneRM);
+/**
+ * @private
+ */
+var singleLiftStrengthScore = function(isMale, age, bodyweight, exerciseName, oneRM) {
+  "use strict";
+  var expectedWilks = wilksCoefficient(isMale, bodyweight) * expectedPLTotal(isMale, bodyweight, exerciseName, oneRM);
   return wilksToStrengthScore(expectedWilks, age);
 };
 
-var getSingleExerciseStrengthScore = function(sex, age, bodyweight, exerciseName, oneRM) {
-  return Math.round(singleLiftStrengthScore(sex, age, bodyweight, exerciseName, oneRM) * 10) / 10;
+/**
+ * @public
+ * Calculates the strength score for a single exercise
+ * @param {boolean} isMale - True, if the value is calculated for a male lifter, false otherwise
+ * @param {integer} age - The age of the lifter in years
+ * @param {integer} bodyweight - The bodyweight of the lifter in kilogram
+ * @param {string} exerciseName - The name of the exercise in lowerCamelCase (e.g. "backSquat")
+ * @param {double} oneRM - The one repitition maximum for the given lift
+ * @return {double}
+ */
+export const getSingleExerciseStrengthScore = (isMale, age, bodyweight, exerciseName, oneRM) => {
+  "use strict";
+  return Math.round(singleLiftStrengthScore(isMale, age, bodyweight, exerciseName, oneRM) * 10) / 10;
 };
